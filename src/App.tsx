e5861e4d3210b444ea76d0a8d40052abfb53a287
alae-herrak <alaeherrak@gmail.com>
2022-11-16 20:27:31 +0100

    progress commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string
+}
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tag: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', []);
+  const [tags, setTages] = useLocalStorage<Tag[]>('TAGS', []);
   return (
     <Container className="my-4">
       <Routes>
